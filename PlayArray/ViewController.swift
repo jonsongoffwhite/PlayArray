be6084976e85ceda054ef13c047f3f3d2aa4ed3e
Louis de Beaumont <debeaumont.louis@googlemail.com>
2016-10-15 15:51:39 +0100

    Initial commit

---
+++ PlayArray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayArray
+//
+//  Created by Louis de Beaumont on 15/10/2016.
+//  Copyright © 2016 PlayArray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
