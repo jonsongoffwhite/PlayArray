2ad9f58014713670e39a6d71a43aa1d0373f299b
Louis de Beaumont <louis.de-beaumont14@imperial.ac.uk>
2016-10-15 17:15:02 +0100

    Create initial Request Manager with Alamofire

---
+++ RequestManager/RequestManager.swift
@@ -0,0 +1,14 @@
+//
+//  RequestManager.swift
+//  PlayArray
+//
+//  Created by Louis de Beaumont on 15/10/2016.
+//  Copyright © 2016 PlayArray. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class RequestManager {
+    
+}
