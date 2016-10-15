7d1918070bebb4d0fca139297c2a76eb8752db35
Jonny Goff-White <jhg14@ic.ac.uk>
2016-10-15 16:06:52 +0100

    Add data structure for representing time of day

---
+++ PlayArray/TimeOfDay.swift
@@ -0,0 +1,19 @@
+//
+//  TimeOfDay.swift
+//  PlayArray
+//
+//  Created by Jonny Goff-White on 15/10/2016.
+//  Copyright © 2016 PlayArray. All rights reserved.
+//
+
+import Foundation
+
+enum TimeOfDay {
+    case Dawn
+    case Morning
+    case Day
+    case Evening
+    case Dusk
+    case Night
+    case LateNight
+}
