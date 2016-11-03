a49f12d68745fe362f4e37730c46ad1d4cf88fb5
Louis de Beaumont <louis.de-beaumont14@imperial.ac.uk>
2016-11-03 23:12:40 +0000

    Get list of Partial Playlists from users account

--- PlayArray/SpotifyManager.swift
+++ PlayArray/SpotifyManager.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class SpotifyManager {
     static let clientID = "ab0607417c0c4a13bb87262583255500"
@@ -34,7 +36,29 @@ class SpotifyManager {
             auth?.handleAuthCallback(withTriggeredAuthURL: url, callback: { (error, session) in
                 self.accessToken = session?.accessToken
                 self.username = session?.canonicalUsername
+                
+                self.getPlaylists()
             })
+        }
+    }
+    
+    func getPlaylists() {
+        let playlistRequest: URLRequest
+        do {
+            playlistRequest = try SPTPlaylistList.createRequestForGettingPlaylists(forUser: username, withAccessToken: accessToken)
+        } catch {
+            print("fuck off \(error)")
+            return
+        }
+        
+        Alamofire.request(playlistRequest)
+        .response { response in
+            do {
+                let list = try SPTPlaylistList(from: response.data, with: response.response)
+                let playlists = list.value(forKey: "items") // SPTPartialPlaylist array
+            } catch {
+                print(error)
+            }
         }
     }
     
