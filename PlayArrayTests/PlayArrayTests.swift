b5f3d55431867f39334292aae9d4ff6288b10915
Jonny Goff-White <jhg14@ic.ac.uk>
2016-11-08 17:44:07 +0000

    Add test that will always fail

--- PlayArrayTests/PlayArrayTests.swift
+++ PlayArrayTests/PlayArrayTests.swift
@@ -26,6 +26,10 @@ class PlayArrayTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    func testWillFail() {
+        XCTAssert(1 == 2)
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
