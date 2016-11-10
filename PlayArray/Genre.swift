f2a4570da2a5308e87a4e307926a8c0f9f8a2a3a
Jono Muller <jonomuller@gmail.com>
2016-11-10 22:38:45 +0000

    Create Genre enum with all parameters from server

---
+++ PlayArray/Genre.swift
@@ -0,0 +1,34 @@
+//
+//  Genre.swift
+//  PlayArray
+//
+//  Created by Jono Muller on 10/11/2016.
+//  Copyright © 2016 PlayArray. All rights reserved.
+//
+
+import Foundation
+
+public enum Genre: String, Criteria {
+    case rock = "rock"
+    case country = "country"
+    case indie = "indie"
+    case raegae = "raegae"
+    case pop = "pop"
+    case christianHymn = "christian hymn"
+    case alternative = "alternative"
+    case soundtracks = "soundtracks"
+    case hipHop = "hip hop"
+    case electronic = "electronic"
+    case newAge = "new age"
+    case neo = "neo"
+    case classical = "classical"
+    case blues = "blues"
+    case international = "international"
+    case rap = "rap"
+    case jazz = "jazz"
+    case ambient = "ambient"
+    case instrumental = "instrumental"
+    case folk = "folk"
+    
+    static let allValues = [Genre.rock, Genre.country, Genre.indie, Genre.raegae, Genre.pop, Genre.christianHymn, Genre.alternative, Genre.soundtracks, Genre.hipHop, Genre.electronic, Genre.newAge, Genre.neo, Genre.classical, Genre.blues, Genre.international, Genre.rap, Genre.jazz, Genre.ambient, Genre.instrumental, Genre.folk]
+}
