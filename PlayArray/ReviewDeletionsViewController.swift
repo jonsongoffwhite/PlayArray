fa70bf1b0e8148964085b4114a0d34a4af8ae8c2
Jonny Goff-White <jhg14@ic.ac.uk>
2016-11-15 21:03:12 +0000

    Implement clicking of Not My Thing button to remove song from deletion review

--- PlayArray/ReviewDeletionsViewController.swift
+++ PlayArray/ReviewDeletionsViewController.swift
@@ -14,18 +14,27 @@ class ReviewDeletionsViewController: UIViewController, UITableViewDataSource, UI
     
     @IBOutlet weak var table: UITableView!
     var alteredSongs: [(Playlist, [Song])] = []
+    var alteredSongsList: [(Playlist, Song)] = []
     
     override func viewDidLoad() {
+        
+        alteredSongs.forEach { (tuple) in
+            let playlist = tuple.0
+            let songs = tuple.1
+            
+            songs.forEach({ (song) in
+                alteredSongsList.append((playlist, song))
+            })
+            
+        }
+        
         self.table.delegate = self
         self.table.dataSource = self
     }
     
     //TODO:
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return alteredSongs.reduce(0, { (r, tuple) -> Int in
-            let songs = tuple.1
-            return r + songs.count
-        })
+        return alteredSongsList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,43 +42,32 @@ class ReviewDeletionsViewController: UIViewController, UITableViewDataSource, UI
         let reuseIdentifier = "programmaticCell"
         let cell = self.table.dequeueReusableCell(withIdentifier: reuseIdentifier) as! MGSwipeTableCell
         
-        let item = getTupleItem(at: indexPath.row)!
+        let item = alteredSongsList[indexPath.row]
         
         cell.textLabel!.text = "\(item.0.name): \(item.1.title) - \(item.1.artist)"
         //cell.detailTextLabel!.text = "Detail text"
         cell.delegate = self //optional
         
         //configure left buttons
-        cell.leftButtons = [MGSwipeButton(title: "Not My Thing", backgroundColor: UIColor(red: 0.596, green: 0.761, blue: 0.38, alpha: 1))]
+        
+        let leftButton = MGSwipeButton(title: "Not My Thing", backgroundColor: UIColor(red: 0.596, green: 0.761, blue: 0.38, alpha: 1))
+        leftButton.callback = { (sender: MGSwipeTableCell!) -> Bool in
+            
+            self.alteredSongsList.remove(at: indexPath.row)
+            
+            self.table.deleteRows(at: [indexPath], with: UITableViewRowAnimation.left)
+            
+            return true
+        }
+        
+        cell.leftButtons = [leftButton]
         
         cell.leftSwipeSettings.transition = MGSwipeTransition.rotate3D
         
         //configure right buttons
         cell.rightButtons = [MGSwipeButton(title: "Not Appropriate", backgroundColor: UIColor(red: 1, green: 0.835, blue: 0, alpha: 1))]
         cell.rightSwipeSettings.transition = MGSwipeTransition.rotate3D
-        
         return cell
         
     }
-}
-
-extension ReviewDeletionsViewController {
-
-    func getTupleItem(at index: Int) -> (Playlist, Song)? {
-        var decounter = index
-        var returnValue: (Playlist, Song)?
-        alteredSongs.forEach { (tuple) in
-            let playlist = tuple.0
-            let songs = tuple.1
-            
-            if decounter - songs.count >= 0 {
-                decounter -= songs.count
-            } else {
-                returnValue = (playlist, songs[decounter])
-                return
-            }
-        }
-        return returnValue
-    }
-    
 }
